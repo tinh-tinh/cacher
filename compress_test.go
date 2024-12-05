41d670236a6857d1a51f8352cc5c348d905209d5
Ren0503 <illuminati0440@gmail.com>
2024-12-05 09:36:41 +0700

    test: set up unit test for v1.1.0

--- compress_test.go
+++ compress_test.go
@@ -1,126 +1,129 @@
-package cacher
+package cacher_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+	"github.com/tinh-tinh/cacher"
 )
 
-func Test_Bytes(t *testing.T) {
-	type Person struct {
-		Name string
-		Age  int
+type BigStruct struct {
+	ID          int64
+	Name        string
+	Description string
+	Timestamp   string
+	Tags        []string
+	Data        []byte
+	Metadata    map[string]interface{}
+	Coordinates struct {
+		Latitude  float64
+		Longitude float64
+	}
+	Attributes struct {
+		IsActive    bool
+		AccessLevel int
+		Notes       []string
+	}
+	Children []struct {
+		ChildID   int
+		ChildName string
+		ChildData []byte
+	}
+}
+
+func fillValue() BigStruct {
+	bigStruct := BigStruct{
+		ID:          123456789,
+		Name:        "ExampleStruct",
+		Description: "This is a large struct for testing purposes.",
+		Timestamp:   time.Now().Format("2006-01-01"),
+		Tags:        []string{"example", "testing", "golang"},
+		Data:        []byte("Random binary data for testing."),
+		Metadata: map[string]interface{}{
+			"key1": "value1",
+			"key2": 42,
+			"key3": []string{"nested", "data"},
+		},
+		Children: []struct {
+			ChildID   int
+			ChildName string
+			ChildData []byte
+		}{
+			{ChildID: 1, ChildName: "Child1", ChildData: []byte("Child1 data")},
+			{ChildID: 2, ChildName: "Child2", ChildData: []byte("Child2 data")},
+		},
 	}
 
-	person := &Person{
-		Name: "John",
-		Age:  30,
-	}
+	// Fill coordinates and attributes
+	bigStruct.Coordinates.Latitude = 37.7749
+	bigStruct.Coordinates.Longitude = -122.4194
+	bigStruct.Attributes.IsActive = true
+	bigStruct.Attributes.AccessLevel = 5
+	bigStruct.Attributes.Notes = []string{"Note1", "Note2", "Note3"}
 
-	data, err := toBytes(person)
-	require.Nil(t, err)
-
-	valRaw, err := fromBytes[Person](data)
-	require.Nil(t, err)
-
-	val, ok := valRaw.(Person)
-	require.True(t, ok)
-	require.Equal(t, person.Age, val.Age)
-	require.Equal(t, person.Name, val.Name)
-
-	_, err = toBytes(nil)
-	require.NotNil(t, err)
-
-	_, err = fromBytes[Person]([]byte("test"))
-	require.NotNil(t, err)
+	return bigStruct
 }
 
 func Test_Gzip(t *testing.T) {
-	type Person struct {
-		Name string
-		Age  int
-	}
-
-	person := Person{
-		Name: "John",
-		Age:  30,
-	}
-
-	data, err := toBytes(person)
+	bigstruct := fillValue()
+	data, err := cacher.ToBytes(bigstruct)
 	require.Nil(t, err)
 
-	val, err := CompressGzip(data)
+	val, err := cacher.CompressGzip(data)
 	require.Nil(t, err)
 
-	valRaw, err := DecompressGzip(val)
+	require.Less(t, len(val), len(data))
+
+	valRaw, err := cacher.DecompressGzip(val)
 	require.Nil(t, err)
 
-	personRaw, err := fromBytes[Person](valRaw)
+	personRaw, err := cacher.FromBytes[BigStruct](valRaw)
 	require.Nil(t, err)
 
-	newPerson, ok := personRaw.(Person)
+	newPerson, ok := personRaw.(BigStruct)
 	require.True(t, ok)
 
-	require.Equal(t, person.Age, newPerson.Age)
-	require.Equal(t, person.Name, newPerson.Name)
+	require.Equal(t, bigstruct, newPerson)
 }
 
 func Test_Flate(t *testing.T) {
-	type Person struct {
-		Name string
-		Age  int
-	}
-
-	person := Person{
-		Name: "John",
-		Age:  30,
-	}
-
-	data, err := toBytes(person)
+	person := fillValue()
+	data, err := cacher.ToBytes(person)
 	require.Nil(t, err)
 
-	val, err := CompressFlate(data)
+	val, err := cacher.CompressFlate(data)
 	require.Nil(t, err)
 
-	valRaw, err := DecompressFlate(val)
+	valRaw, err := cacher.DecompressFlate(val)
 	require.Nil(t, err)
 
-	personRaw, err := fromBytes[Person](valRaw)
+	personRaw, err := cacher.FromBytes[BigStruct](valRaw)
 	require.Nil(t, err)
 
-	newPerson, ok := personRaw.(Person)
+	newPerson, ok := personRaw.(BigStruct)
 	require.True(t, ok)
 
-	require.Equal(t, person.Age, newPerson.Age)
-	require.Equal(t, person.Name, newPerson.Name)
+	require.Equal(t, newPerson, person)
 }
 
 func Test_Zlib(t *testing.T) {
-	type Person struct {
-		Name string
-		Age  int
-	}
+	person := fillValue()
 
-	person := Person{
-		Name: "John",
-		Age:  30,
-	}
-
-	data, err := toBytes(person)
+	data, err := cacher.ToBytes(person)
 	require.Nil(t, err)
 
-	val, err := CompressZlib(data)
+	val, err := cacher.CompressZlib(data)
 	require.Nil(t, err)
 
-	valRaw, err := DecompressZlib(val)
+	valRaw, err := cacher.DecompressZlib(val)
 	require.Nil(t, err)
 
-	personRaw, err := fromBytes[Person](valRaw)
+	personRaw, err := cacher.FromBytes[BigStruct](valRaw)
 	require.Nil(t, err)
 
-	newPerson, ok := personRaw.(Person)
+	newPerson, ok := personRaw.(BigStruct)
 	require.True(t, ok)
 
-	require.Equal(t, person.Age, newPerson.Age)
-	require.Equal(t, person.Name, newPerson.Name)
+	require.Equal(t, person, newPerson)
 }
