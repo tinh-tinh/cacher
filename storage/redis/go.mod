a281abff55332646161c84a08a5606818446519c
Ren0503 <illuminati0440@gmail.com>
2025-04-07 19:33:23 +0700

    feat: sync up cacher v2 with redis storage

--- storage/redis/go.mod
+++ storage/redis/go.mod
@@ -17,6 +17,8 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/tinh-tinh/cacher/v2 v2.2.0 // indirect
+	github.com/tinh-tinh/tinhtinh/v2 v2.0.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
--- storage/redis/redis_test.go
+++ storage/redis/redis_test.go
@@ -3,7 +3,6 @@ package redis_test
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http/httptest"
 	"testing"
@@ -11,14 +10,14 @@ import (
 
 	redis_store "github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/require"
-	"github.com/tinh-tinh/cacher"
 	"github.com/tinh-tinh/cacher/storage/redis"
-	"github.com/tinh-tinh/tinhtinh/common"
-	"github.com/tinh-tinh/tinhtinh/core"
+	"github.com/tinh-tinh/cacher/v2"
+	"github.com/tinh-tinh/tinhtinh/v2/common"
+	"github.com/tinh-tinh/tinhtinh/v2/core"
 )
 
 func Test_Expire(t *testing.T) {
-	cache := redis.New[any](redis.Options{
+	cache := redis.New(redis.Options{
 		Connect: &redis_store.Options{
 			Addr:     "localhost:6379",
 			DB:       0,
@@ -27,14 +26,26 @@ func Test_Expire(t *testing.T) {
 		Ttl: 1 * time.Millisecond,
 	})
 
+	// Auto expired
 	ctx := context.Background()
-	err := cache.Set(ctx, "expire", "John")
+	err := cache.Set(ctx, "expire", []byte("John"))
 	require.Nil(t, err)
 
 	time.Sleep(2 * time.Millisecond)
 	data, err := cache.Get(ctx, "expire")
 	require.Nil(t, err)
 	require.Nil(t, data)
+
+	// Specific expired
+	err = cache.Set(ctx, "spec_expire", []byte("John"), cacher.StoreOptions{
+		Ttl: 1 * time.Second,
+	})
+	require.Nil(t, err)
+
+	time.Sleep(2 * time.Millisecond)
+	data, err = cache.Get(ctx, "spec_expire")
+	require.Nil(t, err)
+	require.NotNil(t, data)
 }
 
 func Test_GetSet(t *testing.T) {
@@ -43,7 +54,7 @@ func Test_GetSet(t *testing.T) {
 		Age  int
 	}
 
-	cache := redis.New[Person](redis.Options{
+	cache := redis.New(redis.Options{
 		Connect: &redis_store.Options{
 			Addr:     "localhost:6379",
 			DB:       0,
@@ -54,48 +65,27 @@ func Test_GetSet(t *testing.T) {
 
 	ctx := context.Background()
 
-	err := cache.Set(ctx, "users", Person{
+	person, err := json.Marshal(Person{
 		Name: "John",
 		Age:  30,
 	})
 	require.Nil(t, err)
+	err = cache.Set(ctx, "users", person)
+	require.Nil(t, err)
 
-	data, err := cache.Get(ctx, "users")
+	raw, err := cache.Get(ctx, "users")
+	require.Nil(t, err)
+
+	var data Person
+	err = json.Unmarshal(raw, &data)
 	require.Nil(t, err)
 
 	require.Equal(t, "John", data.Name)
 	require.Equal(t, 30, data.Age)
 }
 
-func Test_MGetSet(t *testing.T) {
-	cache := redis.New[string](redis.Options{
-		Connect: &redis_store.Options{
-			Addr:     "localhost:6379",
-			DB:       0,
-			Password: "",
-		},
-		Ttl: 15 * time.Minute,
-	})
-
-	ctx := context.Background()
-
-	err := cache.MSet(ctx, cacher.Params[string]{
-		Key: "11",
-		Val: "John",
-	}, cacher.Params[string]{
-		Key: "12",
-		Val: "Jane",
-	})
-	require.Nil(t, err)
-
-	data, err := cache.MGet(ctx, "11", "12")
-	require.Nil(t, err)
-	require.Equal(t, "John", data[0])
-	require.Equal(t, "Jane", data[1])
-}
-
 func Test_Clear(t *testing.T) {
-	cache := redis.New[string](redis.Options{
+	cache := redis.New(redis.Options{
 		Connect: &redis_store.Options{
 			Addr:     "localhost:6379",
 			DB:       0,
@@ -106,88 +96,19 @@ func Test_Clear(t *testing.T) {
 
 	ctx := context.Background()
 
-	err := cache.MSet(ctx, cacher.Params[string]{
-		Key: "1",
-		Val: "John",
-	}, cacher.Params[string]{
-		Key: "2",
-		Val: "Jane",
-	})
+	err := cache.Set(ctx, "1", []byte("John"))
 	require.Nil(t, err)
 
 	err = cache.Clear(ctx)
 	require.Nil(t, err)
 
-	data, err := cache.MGet(ctx, "1", "2")
+	data, err := cache.Get(ctx, "1")
 	require.Nil(t, err)
 	require.Empty(t, data)
 }
 
-func Test_CompressGzip(t *testing.T) {
-	cache := redis.New[string](redis.Options{
-		Connect: &redis_store.Options{
-			Addr:     "localhost:6379",
-			DB:       0,
-			Password: "",
-		},
-		Ttl:         15 * time.Minute,
-		CompressAlg: cacher.CompressAlgGzip,
-	})
-
-	ctx := context.Background()
-
-	err := cache.Set(ctx, "gzip", "John")
-	require.Nil(t, err)
-
-	data, err := cache.Get(ctx, "gzip")
-	require.Nil(t, err)
-	require.Equal(t, "John", data)
-}
-
-func Test_CompressFlate(t *testing.T) {
-	cache := redis.New[string](redis.Options{
-		Connect: &redis_store.Options{
-			Addr:     "localhost:6379",
-			DB:       0,
-			Password: "",
-		},
-		Ttl:         15 * time.Minute,
-		CompressAlg: cacher.CompressAlgFlate,
-	})
-
-	ctx := context.Background()
-
-	err := cache.Set(ctx, "flate", "John")
-	require.Nil(t, err)
-
-	data, err := cache.Get(ctx, "flate")
-	require.Nil(t, err)
-	require.Equal(t, "John", data)
-}
-
-func Test_CompressZlib(t *testing.T) {
-	cache := redis.New[string](redis.Options{
-		Connect: &redis_store.Options{
-			Addr:     "localhost:6379",
-			DB:       0,
-			Password: "",
-		},
-		Ttl:         15 * time.Minute,
-		CompressAlg: cacher.CompressAlgZlib,
-	})
-
-	ctx := context.Background()
-
-	err := cache.Set(ctx, "zlib", "John")
-	require.Nil(t, err)
-
-	data, err := cache.Get(ctx, "zlib")
-	require.Nil(t, err)
-	require.Equal(t, "John", data)
-}
-
 func Test_Delete(t *testing.T) {
-	cache := redis.New[string](redis.Options{
+	cache := redis.New(redis.Options{
 		Connect: &redis_store.Options{
 			Addr:     "localhost:6379",
 			DB:       0,
@@ -198,12 +119,12 @@ func Test_Delete(t *testing.T) {
 
 	ctx := context.Background()
 
-	err := cache.Set(ctx, "delete", "John")
+	err := cache.Set(ctx, "delete", []byte("John"))
 	require.Nil(t, err)
 
 	data, err := cache.Get(ctx, "delete")
 	require.Nil(t, err)
-	require.Equal(t, "John", data)
+	require.Equal(t, []byte("John"), data)
 
 	err = cache.Delete(ctx, "delete")
 	require.Nil(t, err)
@@ -213,72 +134,8 @@ func Test_Delete(t *testing.T) {
 	require.Empty(t, data)
 }
 
-func Test_Fail(t *testing.T) {
-	cache := redis.New[any](redis.Options{
-		Connect: &redis_store.Options{
-			Addr:     "localhost:6379",
-			DB:       0,
-			Password: "",
-		},
-		Ttl:         15 * time.Minute,
-		CompressAlg: "abc",
-	})
-
-	require.Nil(t, cache)
-}
-
-func Test_Hook(t *testing.T) {
-	cache := redis.New[string](redis.Options{
-		Connect: &redis_store.Options{
-			Addr:     "localhost:6379",
-			DB:       0,
-			Password: "",
-		},
-		Ttl: 15 * time.Minute,
-		Hooks: []cacher.Hook{
-			{Key: cacher.BeforeSet, Fnc: func(key string, val any) {
-				fmt.Println("BeforeSet", key, val)
-			}},
-			{Key: cacher.AfterSet, Fnc: func(key string, val any) {
-				fmt.Println("AfterSet", key, val)
-			}},
-			{Key: cacher.BeforeGet, Fnc: func(key string, val any) {
-				fmt.Println("BeforeGet", key)
-			}},
-			{Key: cacher.AfterGet, Fnc: func(key string, val any) {
-				fmt.Println("AfterGet", key, val)
-			}},
-			{Key: cacher.BeforeDelete, Fnc: func(key string, val any) {
-				fmt.Println("BeforeDelete", key)
-			}},
-			{Key: cacher.AfterDelete, Fnc: func(key string, val any) {
-				fmt.Println("AfterDelete", key)
-			}},
-		},
-	})
-
-	ctx := context.Background()
-
-	err := cache.Set(ctx, "before", "John")
-	require.Nil(t, err)
-
-	err = cache.Set(ctx, "after", "Jane")
-	require.Nil(t, err)
-
-	data, err := cache.Get(ctx, "before")
-	require.Nil(t, err)
-	require.Equal(t, "John", data)
-
-	err = cache.Delete(ctx, "before")
-	require.Nil(t, err)
-
-	data, err = cache.Get(ctx, "before")
-	require.Nil(t, err)
-	require.Empty(t, data)
-}
-
 func Test_Module(t *testing.T) {
-	userController := func(module *core.DynamicModule) *core.DynamicController {
+	userController := func(module core.Module) core.Controller {
 		cache := cacher.Inject[[]byte](module)
 		ctrl := module.NewController("users")
 
@@ -303,16 +160,16 @@ func Test_Module(t *testing.T) {
 		return ctrl
 	}
 
-	userModule := func(module *core.DynamicModule) *core.DynamicModule {
+	userModule := func(module core.Module) core.Module {
 		return module.New(core.NewModuleOptions{
-			Controllers: []core.Controller{
+			Controllers: []core.Controllers{
 				userController,
 			},
 		})
 	}
 
-	appModule := func() *core.DynamicModule {
-		cache := redis.New[[]byte](redis.Options{
+	appModule := func() core.Module {
+		cache := redis.New(redis.Options{
 			Connect: &redis_store.Options{
 				Addr:     "localhost:6379",
 				DB:       0,
@@ -322,9 +179,8 @@ func Test_Module(t *testing.T) {
 		})
 
 		module := core.NewModule(core.NewModuleOptions{
-			Imports: []core.Module{
-				cacher.Register(cacher.Options[[]byte]{
-					Ttl:   15 * time.Minute,
+			Imports: []core.Modules{
+				cacher.Register(cacher.Config{
 					Store: cache,
 				}),
 				userModule,
