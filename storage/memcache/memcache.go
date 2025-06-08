a5839dd6bbfb20446507aac2e0ea3af4eb9c30b6
Ren0503 <illuminati0440@gmail.com>
2025-06-08 22:44:56 +0700

    feat: sync memcache with cacher v2

--- storage/memcache/memcache.go
+++ storage/memcache/memcache.go
@@ -2,109 +2,49 @@ package memcache
 
 import (
 	"context"
-	"encoding/json"
-	"reflect"
 	"time"
 
 	memcache_store "github.com/bradfitz/gomemcache/memcache"
-	"github.com/tinh-tinh/cacher"
+	"github.com/tinh-tinh/cacher/v2"
 )
 
 type Options struct {
-	Addr        []string
-	Ttl         time.Duration
-	Hooks       []cacher.Hook
-	CompressAlg cacher.CompressAlg
+	Addr  []string
+	Ttl   time.Duration
+	Hooks []cacher.Hook
 }
 
-func New[M any](opt Options) cacher.Store[M] {
-	if opt.CompressAlg != "" && !cacher.IsValidAlg(opt.CompressAlg) {
-		return nil
-	}
-
+func New[M any](opt Options) cacher.Store {
 	client := memcache_store.New(opt.Addr...)
-	return &Memcache[M]{
-		client:      client,
-		ttl:         opt.Ttl,
-		hooks:       opt.Hooks,
-		CompressAlg: opt.CompressAlg,
+	return &Memcache{
+		client: client,
+		ttl:    opt.Ttl,
 	}
 }
 
-type Memcache[M any] struct {
-	client      *memcache_store.Client
-	ttl         time.Duration
-	hooks       []cacher.Hook
-	CompressAlg cacher.CompressAlg
+type Memcache struct {
+	client *memcache_store.Client
+	ttl    time.Duration
 }
 
-func (m *Memcache[M]) SetOptions(opt cacher.StoreOptions) {
+func (m *Memcache) SetOptions(opt cacher.StoreOptions) {
 	m.ttl = opt.Ttl
-	m.hooks = opt.Hooks
-	m.CompressAlg = opt.CompressAlg
 }
 
-func (m *Memcache[M]) Get(ctx context.Context, key string) (M, error) {
-	cacher.HandlerBeforeGet(m, key)
-
+func (m *Memcache) Get(ctx context.Context, key string) ([]byte, error) {
 	// Handler
 	val, err := m.client.Get(key)
 	if err != nil {
 		if err == memcache_store.ErrCacheMiss {
-			return *new(M), nil
+			return nil, nil
 		}
-		return *new(M), err
+		return nil, err
 	}
 
-	var schema M
-	err = json.Unmarshal(val.Value, &schema)
-	if err != nil {
-		if m.CompressAlg != "" {
-			schema, err = cacher.Decompress[M](val.Value, m.CompressAlg)
-			if err != nil {
-				return *new(M), err
-			}
-		} else {
-			return *new(M), err
-		}
-	}
-
-	cacher.HandlerAfterGet(m, key, schema)
-	return schema, nil
+	return val.Value, nil
 }
 
-func (m *Memcache[M]) MGet(ctx context.Context, keys ...string) ([]M, error) {
-	var output []M
-	for _, key := range keys {
-		schema, err := m.Get(ctx, key)
-		if err != nil {
-			return nil, err
-		}
-		if !reflect.ValueOf(schema).IsZero() {
-			output = append(output, schema)
-		}
-	}
-	return output, nil
-}
-
-func (m *Memcache[M]) Set(ctx context.Context, key string, val M, opts ...cacher.StoreOptions) error {
-	cacher.HandlerBeforeSet(m, key, val)
-
-	// Handler
-	var value []byte
-	valStr, err := json.Marshal(val)
-	if err != nil {
-		return err
-	}
-	value = valStr
-	if m.CompressAlg != "" {
-		b, err := cacher.Compress(val, m.CompressAlg)
-		if err != nil {
-			return err
-		}
-		value = b
-	}
-
+func (m *Memcache) Set(ctx context.Context, key string, val []byte, opts ...cacher.StoreOptions) error {
 	var ttl time.Duration
 	if len(opts) > 0 {
 		ttl = opts[0].Ttl
@@ -112,44 +52,30 @@ func (m *Memcache[M]) Set(ctx context.Context, key string, val M, opts ...cacher
 		ttl = m.ttl
 	}
 
-	err = m.client.Set(&memcache_store.Item{
+	err := m.client.Set(&memcache_store.Item{
 		Key:        key,
-		Value:      value,
+		Value:      val,
 		Expiration: int32(ttl.Seconds()),
 	})
 	if err != nil {
 		return err
 	}
 
-	cacher.HandlerAfterSet(m, key, val)
 	return nil
 }
 
-func (m *Memcache[M]) MSet(ctx context.Context, data ...cacher.Params[M]) error {
-	for _, d := range data {
-		err := m.Set(ctx, d.Key, d.Val)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (m *Memcache[M]) Delete(ctx context.Context, key string) error {
-	cacher.HandlerBeforeDelete(m, key)
+func (m *Memcache) Delete(ctx context.Context, key string) error {
 	// Handler
-	m.client.Delete(key)
+	err := m.client.Delete(key)
+	if err != nil {
+		return err
+	}
 
-	cacher.HandlerAfterDelete(m, key)
 	return nil
 }
 
-func (m *Memcache[M]) Clear(ctx context.Context) error {
+func (m *Memcache) Clear(ctx context.Context) error {
 	// Handler
 	m.client.DeleteAll()
 	return nil
-}
-
-func (m *Memcache[M]) GetHooks() []cacher.Hook {
-	return m.hooks
 }
--- storage/memcache/memcache_test.go
+++ storage/memcache/memcache_test.go
@@ -3,17 +3,16 @@ package memcache_test
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/tinh-tinh/cacher"
 	"github.com/tinh-tinh/cacher/storage/memcache"
-	"github.com/tinh-tinh/tinhtinh/common"
-	"github.com/tinh-tinh/tinhtinh/core"
+	"github.com/tinh-tinh/cacher/v2"
+	"github.com/tinh-tinh/tinhtinh/v2/common"
+	"github.com/tinh-tinh/tinhtinh/v2/core"
 )
 
 func Test_Expire(t *testing.T) {
@@ -23,7 +22,7 @@ func Test_Expire(t *testing.T) {
 	})
 
 	ctx := context.Background()
-	err := cache.Set(ctx, "expire", "John")
+	err := cache.Set(ctx, "expire", []byte("John"))
 	require.Nil(t, err)
 
 	time.Sleep(2 * time.Second)
@@ -45,40 +44,23 @@ func Test_GetSet(t *testing.T) {
 
 	ctx := context.Background()
 
-	err := cache.Set(ctx, "users", Person{
+	person, err := json.Marshal(Person{
 		Name: "John",
 		Age:  30,
 	})
 	require.Nil(t, err)
+	err = cache.Set(ctx, "users", person)
+	require.Nil(t, err)
 
-	data, err := cache.Get(ctx, "users")
+	raw, err := cache.Get(ctx, "users")
+	require.Nil(t, err)
+
+	var data Person
+	err = json.Unmarshal(raw, &data)
 	require.Nil(t, err)
 
 	require.Equal(t, "John", data.Name)
 	require.Equal(t, 30, data.Age)
-}
-
-func Test_MGetSet(t *testing.T) {
-	cache := memcache.New[string](memcache.Options{
-		Addr: []string{"localhost:11211"},
-		Ttl:  15 * time.Minute,
-	})
-
-	ctx := context.Background()
-
-	err := cache.MSet(ctx, cacher.Params[string]{
-		Key: "11",
-		Val: "John",
-	}, cacher.Params[string]{
-		Key: "12",
-		Val: "Jane",
-	})
-	require.Nil(t, err)
-
-	data, err := cache.MGet(ctx, "11", "12")
-	require.Nil(t, err)
-	require.Equal(t, "John", data[0])
-	require.Equal(t, "Jane", data[1])
 }
 
 func Test_Clear(t *testing.T) {
@@ -89,72 +71,15 @@ func Test_Clear(t *testing.T) {
 
 	ctx := context.Background()
 
-	err := cache.MSet(ctx, cacher.Params[string]{
-		Key: "1",
-		Val: "John",
-	}, cacher.Params[string]{
-		Key: "2",
-		Val: "Jane",
-	})
+	err := cache.Set(ctx, "1", []byte("John"))
 	require.Nil(t, err)
 
 	err = cache.Clear(ctx)
 	require.Nil(t, err)
 
-	data, err := cache.MGet(ctx, "1", "2")
+	data, err := cache.Get(ctx, "1")
 	require.Nil(t, err)
 	require.Empty(t, data)
-}
-
-func Test_CompressGzip(t *testing.T) {
-	cache := memcache.New[string](memcache.Options{
-		Addr:        []string{"localhost:11211"},
-		Ttl:         15 * time.Minute,
-		CompressAlg: cacher.CompressAlgGzip,
-	})
-
-	ctx := context.Background()
-
-	err := cache.Set(ctx, "gzip", "John")
-	require.Nil(t, err)
-
-	data, err := cache.Get(ctx, "gzip")
-	require.Nil(t, err)
-	require.Equal(t, "John", data)
-}
-
-func Test_CompressFlate(t *testing.T) {
-	cache := memcache.New[string](memcache.Options{
-		Addr:        []string{"localhost:11211"},
-		Ttl:         15 * time.Minute,
-		CompressAlg: cacher.CompressAlgFlate,
-	})
-
-	ctx := context.Background()
-
-	err := cache.Set(ctx, "flate", "John")
-	require.Nil(t, err)
-
-	data, err := cache.Get(ctx, "flate")
-	require.Nil(t, err)
-	require.Equal(t, "John", data)
-}
-
-func Test_CompressZlib(t *testing.T) {
-	cache := memcache.New[string](memcache.Options{
-		Addr:        []string{"localhost:11211"},
-		Ttl:         15 * time.Minute,
-		CompressAlg: cacher.CompressAlgZlib,
-	})
-
-	ctx := context.Background()
-
-	err := cache.Set(ctx, "zlib", "John")
-	require.Nil(t, err)
-
-	data, err := cache.Get(ctx, "zlib")
-	require.Nil(t, err)
-	require.Equal(t, "John", data)
 }
 
 func Test_Delete(t *testing.T) {
@@ -165,7 +90,7 @@ func Test_Delete(t *testing.T) {
 
 	ctx := context.Background()
 
-	err := cache.Set(ctx, "delete", "John")
+	err := cache.Set(ctx, "delete", []byte("John"))
 	require.Nil(t, err)
 
 	data, err := cache.Get(ctx, "delete")
@@ -180,64 +105,8 @@ func Test_Delete(t *testing.T) {
 	require.Empty(t, data)
 }
 
-func Test_Fail(t *testing.T) {
-	cache := memcache.New[any](memcache.Options{
-		Addr:        []string{"localhost:11211"},
-		Ttl:         15 * time.Minute,
-		CompressAlg: "abc",
-	})
-
-	require.Nil(t, cache)
-}
-
-func Test_Hook(t *testing.T) {
-	cache := memcache.New[string](memcache.Options{
-		Addr: []string{"localhost:11211"},
-		Ttl:  15 * time.Minute,
-		Hooks: []cacher.Hook{
-			{Key: cacher.BeforeSet, Fnc: func(key string, val any) {
-				fmt.Println("BeforeSet", key, val)
-			}},
-			{Key: cacher.AfterSet, Fnc: func(key string, val any) {
-				fmt.Println("AfterSet", key, val)
-			}},
-			{Key: cacher.BeforeGet, Fnc: func(key string, val any) {
-				fmt.Println("BeforeGet", key)
-			}},
-			{Key: cacher.AfterGet, Fnc: func(key string, val any) {
-				fmt.Println("AfterGet", key, val)
-			}},
-			{Key: cacher.BeforeDelete, Fnc: func(key string, val any) {
-				fmt.Println("BeforeDelete", key)
-			}},
-			{Key: cacher.AfterDelete, Fnc: func(key string, val any) {
-				fmt.Println("AfterDelete", key)
-			}},
-		},
-	})
-
-	ctx := context.Background()
-
-	err := cache.Set(ctx, "before", "John")
-	require.Nil(t, err)
-
-	err = cache.Set(ctx, "after", "Jane")
-	require.Nil(t, err)
-
-	data, err := cache.Get(ctx, "before")
-	require.Nil(t, err)
-	require.Equal(t, "John", data)
-
-	err = cache.Delete(ctx, "before")
-	require.Nil(t, err)
-
-	data, err = cache.Get(ctx, "before")
-	require.Nil(t, err)
-	require.Empty(t, data)
-}
-
 func Test_Module(t *testing.T) {
-	userController := func(module *core.DynamicModule) *core.DynamicController {
+	userController := func(module core.Module) core.Controller {
 		cache := cacher.Inject[[]byte](module)
 		ctrl := module.NewController("users")
 
@@ -262,24 +131,23 @@ func Test_Module(t *testing.T) {
 		return ctrl
 	}
 
-	userModule := func(module *core.DynamicModule) *core.DynamicModule {
+	userModule := func(module core.Module) core.Module {
 		return module.New(core.NewModuleOptions{
-			Controllers: []core.Controller{
+			Controllers: []core.Controllers{
 				userController,
 			},
 		})
 	}
 
-	appModule := func() *core.DynamicModule {
+	appModule := func() core.Module {
 		cache := memcache.New[[]byte](memcache.Options{
 			Addr: []string{"localhost:11211"},
 			Ttl:  1 * time.Millisecond,
 		})
 
 		module := core.NewModule(core.NewModuleOptions{
-			Imports: []core.Module{
-				cacher.Register(cacher.Options[[]byte]{
-					Ttl:   15 * time.Minute,
+			Imports: []core.Modules{
+				cacher.Register(cacher.Config{
 					Store: cache,
 				}),
 				userModule,
